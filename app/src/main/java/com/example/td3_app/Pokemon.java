d5f73062e1e147409a749233226449d5a19913a3
martinus01 <63968610+martinus01@users.noreply.github.com>
2020-05-06 18:24:07 +0200

    TD4

---
+++ app/src/main/java/com/example/td3_app/Pokemon.java
@@ -0,0 +1,14 @@
+package com.example.td3_app;
+
+public class Pokemon {
+    private String name;
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
--- app/src/main/java/com/example/td3_app/RestPokemonResponse.java
+++ app/src/main/java/com/example/td3_app/RestPokemonResponse.java
@@ -1,16 +1,21 @@
 package com.example.td3_app;
 
+import java.util.List;
+
 public class RestPokemonResponse {
     private Integer count;
     private String next;
+    private List<Pokemon> results;
 
+    public Integer getCount() {
+        return count;
+    }
 
+    public String getNext() {
+        return next;
+    }
 
-    /*
-    {   "count":964,
-            "next":"https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20",
-            "previous":null,
-            "results":[
-]
-    }*/
+    public List<Pokemon> getResults() {
+        return results;
+    }
 }
