21548af588227c7c72cbec31f62a7e098d835f89
martinus01 <63968610+martinus01@users.noreply.github.com>
2020-05-06 18:15:20 +0200

    TD4

---
+++ app/src/main/java/com/example/td3_app/RestPokemonResponse.java
@@ -0,0 +1,16 @@
+package com.example.td3_app;
+
+public class RestPokemonResponse {
+    private Integer count;
+    private String next;
+
+
+
+    /*
+    {   "count":964,
+            "next":"https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20",
+            "previous":null,
+            "results":[
+]
+    }*/
+}
