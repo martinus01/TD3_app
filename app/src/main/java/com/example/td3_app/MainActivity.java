745ea3cd7c20af2b6ff00283cc5143531fbe8533
martinus01 <63968610+martinus01@users.noreply.github.com>
2020-05-07 13:50:44 +0200

    [features][List] TD5 affichage des donnée reçue du serveur v2

--- app/src/main/java/com/example/td3_app/MainActivity.java
+++ app/src/main/java/com/example/td3_app/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
             showList(pokemonList);
         } else {
             makeApiCall();
-
+            //TODO animation
         }
 
     }
